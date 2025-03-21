0bbd956856f8311c2b4bd17ca6025ad0b371d306
juliocezarcarneiro <carneiro.julio@icloud.com>
2025-03-20 21:10:50 -0400

    Database updated

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -8,27 +8,23 @@ CREATE TABLE "CAMPAIGN" (
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR(255)   NOT NULL,
     "description" TEXT   NOT NULL,
-    "goal" DECIMAL   NOT NULL,
-    "pledged" DECIMAL   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
     "outcome" VARCHAR(50)   NOT NULL,
     "backers_count" INT   NOT NULL,
     "country" VARCHAR(100)   NOT NULL,
     "currency" VARCHAR(10)   NOT NULL,
-    "launch_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "staff_pick" BOOLEAN   NOT NULL,
-    "spotlight" BOOLEAN   NOT NULL,
-    "category" VARCHAR(100)   NOT NULL,
-    "subcategory" VARCHAR(100)   NOT NULL,
-    "category_id" INT   NOT NULL,
-    "subcategory_id" INT   NOT NULL
+    "launch_date" TIMESTAMP   NOT NULL,
+    "end_date" TIMESTAMP   NOT NULL,
+    "category_id" VARCHAR(100)   NOT NULL,
+    "subcategory_id" VARCHAR(100)   NOT NULL
 );
 
 CREATE TABLE "CATEGORY" (
-    "category_id" INT   NOT NULL,
+    "category_id" VARCHAR(100)   NOT NULL,
     "category" VARCHAR(100)   NOT NULL,
     CONSTRAINT "pk_CATEGORY" PRIMARY KEY (
-        "category_id","category"
+        "category_id"
      )
 );
 
@@ -43,19 +39,30 @@ CREATE TABLE "CONTACTS" (
 );
 
 CREATE TABLE "SUBCATEGORY" (
-    "subcategory_id" INT   NOT NULL,
+    "subcategory_id" VARCHAR(100)   NOT NULL,
     "subcategory" VARCHAR(100)   NOT NULL,
     CONSTRAINT "pk_SUBCATEGORY" PRIMARY KEY (
         "subcategory_id","subcategory"
      )
 );
 
-ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "CONTACTS" ("contact_id");
+ALTER TABLE "CATEGORY" ADD CONSTRAINT "fk_CATEGORY_category_id" FOREIGN KEY("category_id")
+REFERENCES "CAMPAIGN" ("category_id");
 
-ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_category_category_id" FOREIGN KEY("category", "category_id")
-REFERENCES "CATEGORY" ("category", "category_id");
+ALTER TABLE "CONTACTS" ADD CONSTRAINT "fk_CONTACTS_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "CAMPAIGN" ("contact_id");
 
-ALTER TABLE "CAMPAIGN" ADD CONSTRAINT "fk_CAMPAIGN_subcategory_subcategory_id" FOREIGN KEY("subcategory", "subcategory_id")
-REFERENCES "SUBCATEGORY" ("subcategory", "subcategory_id");
+ALTER TABLE "SUBCATEGORY" ADD CONSTRAINT "fk_SUBCATEGORY_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "CAMPAIGN" ("subcategory_id");
 
+-- Verify the table creation by running a SELECT statement for each table.
+SELECT * FROM "CONTACTS";
+SELECT * FROM "CATEGORY";
+SELECT * FROM "SUBCATEGORY";
+SELECT * FROM "CAMPAIGN"
+
+-- Verify that each table has the correct data by running a SELECT statement for each.
+SELECT * FROM "CONTACTS";
+SELECT * FROM "CATEGORY";
+SELECT * FROM "SUBCATEGORY";
+SELECT * FROM "CAMPAIGN"
